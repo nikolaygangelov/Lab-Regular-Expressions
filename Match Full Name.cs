e8f44b205f17c1d8c43c7a9a95852a760cb61420
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-17 14:59:07 +0200

    Add files via upload

---
+++ Match Full Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1._Match_Full_Name
+{
+    using System.Text.RegularExpressions;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string listOfNames = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            MatchCollection matches = Regex.Matches(listOfNames, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match.Value} ");
+            }
+        }
+    }
+}
