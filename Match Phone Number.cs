f289852202c3831eb67535cabc6e31eac1fb0568
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-17 17:35:40 +0200

    Add files via upload

---
+++ Match Phone Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"\+\b359( |-)2\1\d{3}\1\d{4}\b";
+            Regex regex = new Regex(pattern);
+            MatchCollection phoneNumbers = regex.Matches(text);
+
+            string[] matchedPhoneNumbers = phoneNumbers.Select(x => x.Value)
+                .ToArray();
+            Console.WriteLine(string.Join(", ", matchedPhoneNumbers));
+        }
+    } 
+}
